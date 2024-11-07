f9fbe33fb43168203a5b7490d8bdc2ed2bc32295
MukulPichunia-Fexle <mukul.pichunia@fexle.com>
2024-11-07 11:00:20 +0530

    Initial branch Student Commit

---
+++ Assignment 1/Student.java
@@ -0,0 +1,10 @@
+class Student {
+    private int id;
+    private String name;
+    public static void main(String[] args) {
+        Student st = new Student()l
+
+        System.out.println(st.name);
+        System.out.println(st.name);
+    }
+}
