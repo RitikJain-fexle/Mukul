ff3ed79fa977b5ec7bd22e8146b99751dee0e651
MukulPichunia-Fexle <mukul.pichunia@fexle.com>
2024-11-07 11:16:17 +0530

    SF commit Bob Changes

---
+++ Assignment 1/Student.java
@@ -0,0 +1,14 @@
+class Student {
+    private int id;
+    private String name;
+    public static void main(String[] args) {
+        Student st = new Student()l
+
+        System.out.println(st.name);
+        System.out.println(st.name);
+    }
+    public void updateStudent(Student st) {
+        Student.name = st.name;
+        System.out.println(st.name);
+    }
+}
