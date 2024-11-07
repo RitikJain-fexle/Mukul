bff7d950cd1d71f9327f71eee6af2f7fb6488ed9
MukulPichunia-Fexle <mukul.pichunia@fexle.com>
2024-11-07 11:13:27 +0530

    CI/CD Commit Alice Changes

---
+++ Assignment 1/Student.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Student {
+    private int id;
+    private String name;
+    public static void main(String[] args) {
+        Student st = new Student()l
+
+        System.out.println(st.name);
+        System.out.println(st.name);
+    }
+    public void addStudent(Student student) {
+        ArrayList<Student> student = new ArrayList<>();
+        System.out.println(student.name);
+    }
+}
